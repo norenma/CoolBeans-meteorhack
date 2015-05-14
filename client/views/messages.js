50fd94dc662d23115c0a605c0629b0a5cb32dd20
Hampus Forsvall <forzvall@gmail.com>
2015-05-14 19:14:18 +0200

    Templates for Received and send messages

--- client/views/messages.js
+++ client/views/messages.js
@@ -3,10 +3,20 @@ Template.sendMessage.events({
         // We are building an application, so we don't want the form to reload the page.
         event.preventDefault();
 
-        var text = instance.find('form').value;
-        instance.find('form').value = '';
+        var text = instance.find('message').value;
+        instance.find('message').value = '';
 
-        Messages.insert({text: text});
+        var to = instance.find('toUser').value;
+        instance.find('toUser').value = '';
+
+        console.log(Meteor.userId());
+
+        Messages.insert({
+          message: text,
+          _fromId: Meteor.userId(), // Add userId to each message.
+          _toId: to,
+          timestamp: new Date()
+        });
 
     }
 });
