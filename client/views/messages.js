460594993cee38c9734228b938c3592ce29ce348
Hampus Forsvall <forzvall@gmail.com>
2015-05-14 20:02:19 +0200

    Attempting to use usernames  in conjunction with messages

--- client/views/messages.js
+++ client/views/messages.js
@@ -1,13 +1,13 @@
 Template.sendMessage.events({
-    'button click': function (event, instance) {
+    'submit form': function (event, instance) {
         // We are building an application, so we don't want the form to reload the page.
         event.preventDefault();
 
-        var text = instance.find('message').value;
-        instance.find('message').value = '';
+        var text = instance.find('#message').value;
+        instance.find('#message').value = '';
 
-        var to = instance.find('toUser').value;
-        instance.find('toUser').value = '';
+        var to = instance.find('#toUser').value;
+        instance.find('#toUser').value = '';
 
         console.log(Meteor.userId());
 
