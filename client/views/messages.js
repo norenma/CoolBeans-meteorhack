9fba80ab50e31c6b52a4a749aa0f1ee6d68ecc88
Hampus Forsvall <forzvall@gmail.com>
2015-05-15 10:39:01 +0200

    Can now send specific songs

--- client/views/messages.js
+++ client/views/messages.js
@@ -3,14 +3,16 @@ Template.sendMessage.events({
         // We are building an application, so we don't want the form to reload the page.
         event.preventDefault();
 
-        var bestSongEver = '5nNmj1cLH3r4aA4XDJ2bgY'
+        var song = instance.find('#trackId').value;
+        instance.find('#trackId').value = '';
+
         var to = instance.find('#toUser').value;
         instance.find('#toUser').value = '';
 
         console.log(Meteor.userId());
 
         Messages.insert({
-          message: bestSongEver,
+          message: song,
           _fromId: Meteor.userId(), // Add userId to each message.
           _toId: to,
           timestamp: new Date()
